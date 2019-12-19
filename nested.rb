16ab48ff801df049c413dd17a5279ea66fe27061
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-19 19:12:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,8 +91,10 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto][:known_for]= "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages]= ["LISP","C"]
+programmer_hash[:yukihiro_matsumoto]= known_for:"Ruby"
+programmer_hash[:yukihiro_matsumoto]= languages: ["LISP","C"]
+
+
 end
 
 def changing_alan
